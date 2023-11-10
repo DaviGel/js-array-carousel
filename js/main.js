fe8b4f5b3d3d7589b511b212a32063aff7223161
DaviGel <geloso.davide@gmail.com>
2023-11-10 19:50:33 +0100

    feat: milestone 1 e 2 bonus 1 e 2

--- js/main.js
+++ js/main.js
@@ -1,25 +1,64 @@
-/*
-<div class="container">
-  <div class="items">
-    <div class="item active">
-      <img src="./img/01.jpg" alt="" />
-    </div>
-    <div class="prev"></div>
-    <div class="next"></div>
-  </div>
-</div>
-*/
-
 const imagesArray = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
 const items = document.querySelector('.items');
+const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
+const array = [];
+const thumbnailsArray = [];
+let counter = 0;
+let counterThumbnails = 0;
+let index = 0;
+const thumbnailsContainer = document.createElement('div');
+thumbnailsContainer.classList.add('thumbnailsContainer');
 
 for (let i = 0; i < imagesArray.length; i++) {
   const element = document.createElement('div');
   element.classList.add('item');
   element.innerHTML = `<img src = "img/${imagesArray[i]}" alt = "img${i}">`;
+  items.append(element);
+  array.push(element);
+  const thumbnail = document.createElement('div');
+  thumbnail.classList.add('thumbnail');
+  thumbnail.innerHTML = `<img src = "img/${imagesArray[i]}" alt = "img${i}">`;
+  thumbnailsContainer.append(thumbnail);
+  items.append(thumbnailsContainer);
+  thumbnailsArray.push(thumbnail);
   if (i === 0) {
     element.classList.add('active');
+  } else {
+    thumbnail.classList.add('overlay');
   }
-  items.append(element);
 }
+
+const item = document.querySelector('.item');
+
+next.addEventListener('click', function () {
+  if (counter < array.length - 1) {
+    array[counter].classList.remove('active');
+    thumbnailsArray[counter].classList.add('overlay');
+    counter++;
+    array[counter].classList.add('active');
+    thumbnailsArray[counter].classList.remove('overlay');
+  } else {
+    array[counter].classList.remove('active');
+    thumbnailsArray[counter].classList.add('overlay');
+    counter = 0;
+    array[counter].classList.add('active');
+    thumbnailsArray[counter].classList.remove('overlay');
+  }
+});
+
+prev.addEventListener('click', function () {
+  if (counter > 0) {
+    array[counter].classList.remove('active');
+    thumbnailsArray[counter].classList.add('overlay');
+    counter--;
+    array[counter].classList.add('active');
+    thumbnailsArray[counter].classList.remove('overlay');
+  } else {
+    array[counter].classList.remove('active');
+    thumbnailsArray[counter].classList.add('overlay');
+    counter = array.length - 1;
+    array[counter].classList.add('active');
+    thumbnailsArray[counter].classList.remove('overlay');
+  }
+});
