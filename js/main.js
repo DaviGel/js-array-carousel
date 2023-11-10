5d9b38114ede1709b6e31fccffc7928e45e129de
DaviGel <geloso.davide@gmail.com>
2023-11-10 17:05:55 +0100

    feat: milestone1

--- js/main.js
+++ js/main.js
@@ -11,14 +11,8 @@
 */
 
 const imagesArray = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
-
-const container = document.createElement('div');
-container.classList.add('container');
-
-const items = document.createElement('div');
-items.classList.add('items');
-
-container.append(items);
+const items = document.querySelector('.items');
+const prev = document.querySelector('.prev');
 
 for (let i = 0; i < imagesArray.length; i++) {
   const element = document.createElement('div');
